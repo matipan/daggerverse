c3e082566aa7dc6f14de00d1241fceee8798bf3a
Matias Pan <matipan@hey.com>
2024-05-28 06:56:17 -0300

    Update image-updater module
    Signed-off-by: Matias Pan <matipan@hey.com>

--- image-updater/dagger/dagger.gen.go
+++ image-updater/dagger/dagger.gen.go
@@ -6749,59 +6749,88 @@ func invoke(ctx context.Context, parentJSON []byte, parentName string, fnName st
 	switch parentName {
 	case "ImageUpdater":
 		switch fnName {
-		case "ContainerEcho":
+		case "Update":
 			var parent ImageUpdater
 			err = json.Unmarshal(parentJSON, &parent)
 			if err != nil {
 				panic(fmt.Errorf("%s: %w", "failed to unmarshal parent object", err))
 			}
-			var stringArg string
-			if inputArgs["stringArg"] != nil {
-				err = json.Unmarshal([]byte(inputArgs["stringArg"]), &stringArg)
+			var repo string
+			if inputArgs["repo"] != nil {
+				err = json.Unmarshal([]byte(inputArgs["repo"]), &repo)
 				if err != nil {
-					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg stringArg", err))
+					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg repo", err))
 				}
 			}
-			return (*ImageUpdater).ContainerEcho(&parent, stringArg), nil
-		case "GrepDir":
-			var parent ImageUpdater
-			err = json.Unmarshal(parentJSON, &parent)
-			if err != nil {
-				panic(fmt.Errorf("%s: %w", "failed to unmarshal parent object", err))
-			}
-			var directoryArg *Directory
-			if inputArgs["directoryArg"] != nil {
-				err = json.Unmarshal([]byte(inputArgs["directoryArg"]), &directoryArg)
+			var branch string
+			if inputArgs["branch"] != nil {
+				err = json.Unmarshal([]byte(inputArgs["branch"]), &branch)
 				if err != nil {
-					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg directoryArg", err))
+					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg branch", err))
 				}
 			}
-			var pattern string
-			if inputArgs["pattern"] != nil {
-				err = json.Unmarshal([]byte(inputArgs["pattern"]), &pattern)
+			var deployFilepath string
+			if inputArgs["deployFilepath"] != nil {
+				err = json.Unmarshal([]byte(inputArgs["deployFilepath"]), &deployFilepath)
 				if err != nil {
-					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg pattern", err))
+					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg deployFilepath", err))
 				}
 			}
-			return (*ImageUpdater).GrepDir(&parent, ctx, directoryArg, pattern)
+			var imageUrl string
+			if inputArgs["imageUrl"] != nil {
+				err = json.Unmarshal([]byte(inputArgs["imageUrl"]), &imageUrl)
+				if err != nil {
+					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg imageUrl", err))
+				}
+			}
+			var gitUser string
+			if inputArgs["gitUser"] != nil {
+				err = json.Unmarshal([]byte(inputArgs["gitUser"]), &gitUser)
+				if err != nil {
+					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg gitUser", err))
+				}
+			}
+			var gitEmail string
+			if inputArgs["gitEmail"] != nil {
+				err = json.Unmarshal([]byte(inputArgs["gitEmail"]), &gitEmail)
+				if err != nil {
+					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg gitEmail", err))
+				}
+			}
+			var gitPassword *Secret
+			if inputArgs["gitPassword"] != nil {
+				err = json.Unmarshal([]byte(inputArgs["gitPassword"]), &gitPassword)
+				if err != nil {
+					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg gitPassword", err))
+				}
+			}
+			var forceWithLease bool
+			if inputArgs["forceWithLease"] != nil {
+				err = json.Unmarshal([]byte(inputArgs["forceWithLease"]), &forceWithLease)
+				if err != nil {
+					panic(fmt.Errorf("%s: %w", "failed to unmarshal input arg forceWithLease", err))
+				}
+			}
+			return nil, (*ImageUpdater).Update(&parent, ctx, repo, branch, deployFilepath, imageUrl, gitUser, gitEmail, gitPassword, forceWithLease)
 		default:
 			return nil, fmt.Errorf("unknown function %s", fnName)
 		}
 	case "":
 		return dag.Module().
 			WithObject(
-				dag.TypeDef().WithObject("ImageUpdater").
+				dag.TypeDef().WithObject("ImageUpdater", TypeDefWithObjectOpts{Description: "image-updater is a dagger module that updates, commits and pushes a kubernetes deployment file with a new image-url.\n\nThere are many alternatives to doing GitOps with kubernetes now a days, to name a few:\n\n- Automatically update the deployment with the new image using Kubectl or hitting the kubernetes API directly (no specific trace of the image deployed in the git repository)\n- Use tools such as Flux or ArgoCD to automatically watch a registry and deploy new images when they appear\n- Use Flux or ArgoCD as well but instead have them look for changes on specific manifests in a repository\n\nThis module is useful for the last alternative. When you have CD tools and that are watching kubernetes manifests on your\nrepository you would need to change them explicitly. If you use Github or Gitlab there are actions that you can use to make\nthis changes (for example, there is a yq action and a git-auto-commit action), but the problem is that those workflows\ncannot be tested locally and they become complicated. In the case of github actions, if you run your action as part of a\nworkflow that takes a long time to run, it might happen that a new commit showed up and your push will fail. Solving this\nis possible, but it requires adding even more untestable bash. This is why this module exists. With image-updater you can\nimplement this logic in a single step that is reproducible locally."}).
 					WithFunction(
-						dag.Function("ContainerEcho",
-							dag.TypeDef().WithObject("Container")).
-							WithDescription("example usage: \"dagger call container-echo --string-arg yo stdout\"").
-							WithArg("stringArg", dag.TypeDef().WithKind(StringKind))).
-					WithFunction(
-						dag.Function("GrepDir",
-							dag.TypeDef().WithKind(StringKind)).
-							WithDescription("example usage: \"dagger call grep-dir --directory-arg . --pattern GrepDir\"").
-							WithArg("directoryArg", dag.TypeDef().WithObject("Directory")).
-							WithArg("pattern", dag.TypeDef().WithKind(StringKind)))), nil
+						dag.Function("Update",
+							dag.TypeDef().WithKind(VoidKind).WithOptional(true)).
+							WithDescription("Update updates the kubernetes deployment file in the specified repository\nwith the new image URL.\nNOTE: this pushes a commit to your repository so make sure that you either\ndon't have a cyclic workflow trigger or that you use a token that prevents\nthis from happening.\n+optional forceWithLease").
+							WithArg("repo", dag.TypeDef().WithKind(StringKind)).
+							WithArg("branch", dag.TypeDef().WithKind(StringKind)).
+							WithArg("deployFilepath", dag.TypeDef().WithKind(StringKind)).
+							WithArg("imageUrl", dag.TypeDef().WithKind(StringKind)).
+							WithArg("gitUser", dag.TypeDef().WithKind(StringKind)).
+							WithArg("gitEmail", dag.TypeDef().WithKind(StringKind)).
+							WithArg("gitPassword", dag.TypeDef().WithObject("Secret")).
+							WithArg("forceWithLease", dag.TypeDef().WithKind(BooleanKind)))), nil
 	default:
 		return nil, fmt.Errorf("unknown object %s", parentName)
 	}
