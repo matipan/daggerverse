2664e98d1c4cb699c88ab591fa88543e0c5b5f38
Matias Pan <gh@matiaspan.dev>
2024-09-24 14:54:54 -0300

    Bump dagger to v0.13.3
    Signed-off-by: Matias Pan <gh@matiaspan.dev>

--- eksctl/main.go
+++ eksctl/main.go
@@ -1,24 +1,39 @@
+// A generated module for Eksctl functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
 package main
 
 import (
 	"context"
+	"dagger/eksctl/internal/dagger"
 	"fmt"
 )
 
 type Eksctl struct {
-	Cluster   *File
-	Container *Container
+	Cluster   *dagger.File
+	Container *dagger.Container
 }
 
 func New(
 	// +optional
 	// +default="latest"
 	version string,
-	awsCreds *Secret,
+	awsCreds *dagger.Secret,
 	awsProfile string,
 	// +optional
-	awsConfig *File,
-	cluster *File) *Eksctl {
+	awsConfig *dagger.File,
+	cluster *dagger.File) *Eksctl {
 	return &Eksctl{
 		Cluster:   cluster,
 		Container: eksctl(version, awsCreds, awsProfile, awsConfig, cluster),
@@ -30,7 +45,7 @@ func New(
 // top of it. This is the unsafe alternative to something like accepting a
 // function as a parameter that modifies the existing container.
 // See https://github.com/dagger/dagger/issues/6213 for more details.
-func (m *Eksctl) WithContainer(ctr *Container) *Eksctl {
+func (m *Eksctl) WithContainer(ctr *dagger.Container) *Eksctl {
 	m.Container = ctr
 	return m
 }
@@ -58,13 +73,13 @@ func (m *Eksctl) Delete(ctx context.Context,
 
 // Kubeconfig returns the kubeconfig of the cluster. To download it using Dagger's
 // CLI you can call `dagger download`.
-func (m *Eksctl) Kubeconfig(ctx context.Context) *File {
+func (m *Eksctl) Kubeconfig(ctx context.Context) *dagger.File {
 	return m.Container.
 		WithExec([]string{"utils", "write-kubeconfig", "-f", "/cluster.yaml", "--kubeconfig", "/kubeconfig.yaml"}).
 		File("/kubeconfig.yaml")
 }
 
-func eksctl(version string, awsCreds *Secret, awsProfile string, awsConfig *File, cluster *File) *Container {
+func eksctl(version string, awsCreds *dagger.Secret, awsProfile string, awsConfig *dagger.File, cluster *dagger.File) *dagger.Container {
 	var asset string
 	if version == "latest" {
 		asset = "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz"
@@ -82,7 +97,7 @@ func eksctl(version string, awsCreds *Secret, awsProfile string, awsConfig *File
 		WithExec([]string{"curl", "-sL", "-o", "/bin/aws-iam-authenticator", "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.14/aws-iam-authenticator_0.6.14_linux_amd64"}).
 		WithExec([]string{"chmod", "+x", "/bin/aws-iam-authenticator"}).
 		WithMountedSecret("/root/.aws/credentials", awsCreds).
-		With(func(c *Container) *Container {
+		With(func(c *dagger.Container) *dagger.Container {
 			if awsConfig != nil {
 				return c.WithFile("/root/.aws/config", awsConfig)
 			}
